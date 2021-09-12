9fb51bfe63ad970e35bb7aa3d5a9f005b01bc704
Bredinn <bredinn2001@gmail.com>
2021-09-12 13:07:09 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/service.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/service.java
@@ -1,5 +1,13 @@
 package com.devsuperior.dsvendas.controllers;
 
+import java.util.List;
+
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
+
 public class service {
+
+	public static List<SaleSumDTO> amountGropredBySeller() {
+		return null;
+	}
 
 }
--- backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -1,42 +1,39 @@
 package com.devsuperior.dsvendas.dto;
 
-import java.io.Serializable;
-
 import com.devsuperior.dsvendas.entities.Seller;
 
-public class SellerDTO implements Serializable{
-	private static final long serialVersionUID = 1L;
-	private Long id;
-	private String name;
-	
-	public SellerDTO() {
-		
-	}
+import java.io.Serializable;
 
-	public SellerDTO(Long id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-	
-	public SellerDTO(Seller entity) {
-		this.id = entity.getId();
-		this.name = entity.getName();
-	}
+public class SellerDTO implements Serializable {
 
-	public Long getId() {
-		return id;
-	}
+    private Long id;
+    private String name;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public SellerDTO(){};
 
-	public String getName() {
-		return name;
-	}
+    public SellerDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
-	
+    public SellerDTO(Seller entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
